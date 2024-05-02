eb30a2e433cfb4b2e57f41a8a3534f934ca3c963
Soul <joshgingold336@gmail.com>
2024-05-02 13:10:05 -0500

    initial commit

---
+++ routes/notes.js
@@ -0,0 +1,9 @@
+const pageRouter = require("express").Router();
+
+pageRouter.get("/", (req, res) => {
+  // path.join(__dirname, "../public/index.html");
+});
+
+pageRouter.post();
+
+pageRouter.delete();
